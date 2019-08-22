d5860e893753edcaed332abff30c69f9865e64ac
Steven Mi <stevenmi@Stevens-MacBook-Pro-2.local>
2019-08-22 16:28:19 +0200

    push

---
+++ create_dataset.sh
@@ -0,0 +1,3 @@
+python object_detection/dataset_tools/create_helmet_tf_record.py --data_dir=/helmet-dataset/data --label_map_path=/helmet-dataset/data/helmet_label_map.pbtxt --set=train --output_path=/helmet-dataset/data/helmet_train.record
+#python object_detection/dataset_tools/create_helmet_tf_record.py --set=test --data_dir=/helmet-dataset/data --label_map_path=/helmet-dataset/data/helmet_label_map.pbtxt --output_path=/helmet-dataset/data/helmet_val.record
+
